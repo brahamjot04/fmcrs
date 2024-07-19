aebe57be631f376344fd586844763d02afbb932e
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-07-19 14:08:52 +0530

    chore: Refactor dashboard/index.php for improved UI and added navbar

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,13 +4,13 @@ if (!isset($_SESSION['username']) || $_SESSION['loggedin'] !== true || !isset($_
     header('Location: ../');
     exit();
 }
-echo $_SESSION['username'];
-echo "<br>";
-echo $_SESSION['id'];
-echo "<br>";
-echo $_SESSION['loggedin'];
-echo "<br>";
-echo "Admin Dashboard";
+// echo $_SESSION['username'];
+// echo "<br>";
+// echo $_SESSION['id'];
+// echo "<br>";
+// echo $_SESSION['loggedin'];
+// echo "<br>";
+// echo "Admin Dashboard";
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +26,47 @@ echo "Admin Dashboard";
 </head>
 
 <body>
-    <a href="../logout/" class="btn btn-primary">Logout</a>
+    <!-- Navbar Started -->
+    <div class="navbar bg-body-tertiary">
+        <div class="container-fluid d-flex justify-content-between">
+            <div class="navbar-brand">
+                <div class="d-flex align-items-center">
+                    <img src="../assets/img/fmcrs.png" alt="" class="d-none d-md-block" width="45" />
+                    <img src="../assets/img/fmcrs.png" alt="" class="d-md-none d-sm-block" width="40" />
+                    <span class="h4 ms-3 pt-2 d-none d-md-block">90.8 MHz FMCRS</span>
+                    <span class="h5 ms-3 pt-2 d-sm-block d-md-none">90.8 MHz FMCRS</span>
+                </div>
+            </div>
+            <span class="d-inline-flex align-items-center">
+                <span class="me-3">Welcome, <?php echo $_SESSION['username']; ?></span>
+                <a href="../logout/" class="btn btn-primary d-none d-md-inline-block">Logout</a>
+                <a href="../logout/" class="btn btn-primary d-sm-block d-md-none btn-sm">Logout</a>
+            </span>
+        </div>
+    </div>
+    <!-- Navbar Ended -->
+
+    <!-- Main Content Started -->
+    <div class="container mt-5">
+        <div class="d-flex p-3 justify-content-center rounded-3 bg-body-tertiary">
+            <div class="h3">
+                Welcome, <?php echo $_SESSION['username'] ?>, to the Admin Panel
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title text-center">Add Users</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Add a new user to the system.</p>
+                        <a href="../add-user/" class="btn btn-primary">Add User</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
