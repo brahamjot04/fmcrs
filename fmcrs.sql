05229c74fac0cd960ae75c4c6cb61d6d72f1521f
Brahamjot Singh <brahamjot2221146@gndec.ac.in>
2024-07-23 16:17:40 +0530

    updated sql file

--- fmcrs.sql
+++ fmcrs.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 22, 2024 at 05:36 PM
+-- Generation Time: Jul 23, 2024 at 10:47 AM
 -- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- PHP Version: 8.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -59,15 +59,18 @@ CREATE TABLE `users` (
   `sno` int NOT NULL,
   `Name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `admin_type` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`sno`, `Name`, `username`, `password`) VALUES
-(7, 'Brahamjot Singh', 'test', '$2y$10$LF.HaFreM15YoavT8c/OluWpRepbx78wKrvSkcoA/DjyR4BYDUyfu');
+INSERT INTO `users` (`sno`, `Name`, `username`, `password`, `admin_type`) VALUES
+(7, 'Brahamjot Singh', 'test', '$2y$10$LF.HaFreM15YoavT8c/OluWpRepbx78wKrvSkcoA/DjyR4BYDUyfu', 1),
+(16, 'user', 'admin@fmcrs', '$2y$10$UhAGPJwZz7hG1oX/FeCiiOIdVdgFvoXIaiUq/v9DP6uaPpvzwTty6', 0),
+(17, 'Test 2', 'test1', '$2y$10$mRaN3DS60E3KnZ0t5PVRauZlgjABEThB/xHi3SkFStNVD8H7pXrnW', 1);
 
 --
 -- Indexes for dumped tables
@@ -108,7 +111,7 @@ ALTER TABLE `faculty_members`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `sno` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `sno` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
